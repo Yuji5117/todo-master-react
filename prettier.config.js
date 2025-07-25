1398a7e77c9e3771b4b990abf73556b4e2f400b3
Yuji Nakamura <ny5117@icloud.com>
2025-07-25 22:22:22 +0800

    setup prettier

---
+++ prettier.config.js
@@ -0,0 +1,16 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: "all",
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  printWidth: 100,
+  bracketSpacing: true,
+  arrowParens: "avoid",
+  endOfLine: "lf",
+};
+
+export default config;
