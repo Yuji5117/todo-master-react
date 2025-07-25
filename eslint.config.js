063198ca3c05d3a38f54d320b0d956b1468dd902
Yuji Nakamura <ny5117@icloud.com>
2025-07-25 23:12:32 +0800

    remove eslint-plugin-prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,22 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import importPlugin from "eslint-plugin-import";
-import prettier from "eslint-plugin-prettier";
-import prettierConfig from "eslint-config-prettier";
-import pluginUnusedImports from "eslint-plugin-unused-imports";
-import tseslint from "typescript-eslint";
-import { globalIgnores } from "eslint/config";
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import importPlugin from 'eslint-plugin-import'
+import prettierConfig from 'eslint-config-prettier'
+import pluginUnusedImports from 'eslint-plugin-unused-imports'
+import tseslint from 'typescript-eslint'
+import { globalIgnores } from 'eslint/config'
 
 export default tseslint.config([
-  globalIgnores(["dist"]),
+  globalIgnores(['dist']),
   {
-    files: ["**/*.{ts,tsx}"],
-    plugins: { "unused-imports": pluginUnusedImports, prettier },
+    files: ['**/*.{ts,tsx}'],
+    plugins: { 'unused-imports': pluginUnusedImports },
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
-      reactHooks.configs["recommended-latest"],
+      reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
       importPlugin.flatConfigs.recommended,
       importPlugin.flatConfigs.typescript,
@@ -27,49 +26,47 @@ export default tseslint.config([
       globals: globals.browser,
     },
     rules: {
-      "import/order": [
-        "error",
+      'import/order': [
+        'error',
         {
-          "newlines-between": "always",
+          'newlines-between': 'always',
           groups: [
-            "builtin",
-            "external",
-            "internal",
-            ["parent", "sibling", "index"],
-            "object",
-            "type",
+            'builtin',
+            'external',
+            'internal',
+            ['parent', 'sibling', 'index'],
+            'object',
+            'type',
           ],
-          pathGroups: [
-            { pattern: "@components/**", group: "internal", position: "after" },
-          ],
-          pathGroupsExcludedImportTypes: ["builtin", "object"],
+          pathGroups: [{ pattern: '@components/**', group: 'internal', position: 'after' }],
+          pathGroupsExcludedImportTypes: ['builtin', 'object'],
           alphabetize: {
-            order: "asc",
+            order: 'asc',
             caseInsensitive: true,
           },
         },
       ],
-      "import/newline-after-import": ["error", { count: 1 }],
-      "unused-imports/no-unused-imports": "error",
-      "import/no-default-export": "error",
-      "unused-imports/no-unused-vars": [
-        "error",
+      'import/newline-after-import': ['error', { count: 1 }],
+      'unused-imports/no-unused-imports': 'error',
+      'import/no-default-export': 'error',
+      'unused-imports/no-unused-vars': [
+        'error',
         {
-          vars: "all",
-          varsIgnorePattern: "^_",
-          args: "after-used",
-          argsIgnorePattern: "^_",
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
         },
       ],
-      "@typescript-eslint/no-unused-vars": "off",
-      "@typescript-eslint/explicit-module-boundary-types": "error",
-      "@typescript-eslint/consistent-type-imports": "error",
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'error',
+      '@typescript-eslint/consistent-type-imports': 'error',
     },
     settings: {
-      "import/resolver": {
+      'import/resolver': {
         typescript: {},
       },
     },
   },
   prettierConfig,
-]);
+])
