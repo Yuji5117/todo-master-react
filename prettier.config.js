2f5bc6dfa55eb0feef2959dab3a49cdcf30bc9e5
Yuji Nakamura <ny5117@icloud.com>
2025-07-26 10:07:55 +0800

    setup prettier-plugin-tailwindcss to format tailwind calss order

--- prettier.config.js
+++ prettier.config.js
@@ -2,14 +2,15 @@
  * @type {import("prettier").Config}
  */
 const config = {
-  trailingComma: "all",
+  trailingComma: 'all',
   tabWidth: 2,
   semi: false,
   singleQuote: true,
   printWidth: 100,
   bracketSpacing: true,
-  arrowParens: "avoid",
-  endOfLine: "lf",
-};
+  arrowParens: 'avoid',
+  endOfLine: 'lf',
+  plugins: ['prettier-plugin-tailwindcss'],
+}
 
-export default config;
+export default config
