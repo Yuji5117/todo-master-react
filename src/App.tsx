0f396d35a3a2c57e8e77e3b3d3e033e40781b8f9
Yuji Nakamura <ny5117@icloud.com>
2025-07-28 20:02:29 +0800

    display and style TodoCard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
+import { TodoCard } from './components/TodoCard'
+
+import type { Todo } from './types'
+
+const dummyTodos: Todo[] = Array.from({ length: 20 }, (_, i) => ({
+  id: (i + 1).toString(),
+  createdAt: Date.now() - i * 1000 * 60,
+  title: `Todo Item ${i + 1}`,
+  memo: `This is a memo for Todo ${i + 1}`,
+  isCompleted: i % 2 === 0,
+}))
+
 export const App: React.FC = () => {
   return (
-    <>
-      <div className="text-red-500">Todo Master</div>
-    </>
+    <div className="flex min-h-screen items-center justify-center">
+      <div className="m-auto flex h-[620px] w-2/4 flex-col rounded-md border border-[#E5E7EB] shadow-md">
+        <div className="bg-primary flex h-12 items-center justify-center">
+          <h1 className="text-xl font-semibold text-white">Todo Master</h1>
+        </div>
+        <div className="flex-1 flex-col space-y-4 overflow-auto py-8">
+          {dummyTodos.map(todo => (
+            <div key={todo.id}>
+              <TodoCard title={todo.title} memo={todo.memo} isCompleted={todo.isCompleted} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
   )
 }
+
