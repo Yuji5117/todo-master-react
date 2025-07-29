3276bdcae656b060e8829c184880a85908d734ac
Yuji Nakamura <ny5117@icloud.com>
2025-07-29 18:37:21 +0800

    add create todo api

--- src/api/todo.ts
+++ src/api/todo.ts
@@ -1,6 +1,6 @@
 import { paths } from '../config/paths'
 
-import type { ApiResponse, Todo } from '../types'
+import type { ApiResponse, CreateNewTodo, Todo } from '../types'
 
 export const getTodos = async (): Promise<ApiResponse<Todo[]>> => {
   try {
@@ -14,5 +14,24 @@ export const getTodos = async (): Promise<ApiResponse<Todo[]>> => {
   } catch (error) {
     console.log('getTodos error:', error)
     throw new Error('Failed to fetch todos')
+  }
+}
+
+export const createNewTodo = async (newTodo: CreateNewTodo): Promise<ApiResponse<Todo>> => {
+  try {
+    const response = await fetch(paths.app.todos.path, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newTodo),
+    })
+
+    const decodedData: ApiResponse<Todo> = await response.json()
+
+    return decodedData
+  } catch (error) {
+    console.log('getTodos error:', error)
+    throw new Error('Failed to create new todo')
   }
 }
