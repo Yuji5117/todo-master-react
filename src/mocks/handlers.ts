381a1c19968d0be19d8b574fd401b4f15728236b
Yuji Nakamura <ny5117@icloud.com>
2025-07-29 17:49:46 +0800

    implement add todo

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,53 +1,66 @@
 import { http, HttpResponse } from 'msw'
+import { v4 as uuid } from 'uuid'
+
+import { paths } from '../config/paths'
 
 import type { Todo } from '../types'
 
+type CreateNewTodo = Omit<Todo, 'id'>
+
+const todos: Todo[] = [
+  {
+    id: '1',
+    title: 'Buy groceries',
+    memo: 'Milk, eggs, bread, and orange juice',
+    isCompleted: false,
+    createdAt: new Date(1722200000000).toISOString(),
+  },
+  {
+    id: '2',
+    title: 'Send project update',
+    memo: 'Email the client about the current status and upcoming tasks',
+    isCompleted: true,
+    createdAt: new Date(1722201000000).toISOString(),
+  },
+  {
+    id: '3',
+    title: 'Workout',
+    memo: '1 hour at the gym: cardio and legs',
+    isCompleted: false,
+    createdAt: new Date(1722202000000).toISOString(),
+  },
+  {
+    id: '4',
+    title: 'Call mom',
+    memo: 'Check in and ask about weekend plans',
+    isCompleted: true,
+    createdAt: new Date(1722203000000).toISOString(),
+  },
+  {
+    id: '5',
+    title: 'Prepare meeting slides',
+    memo: 'Include Q2 results and marketing roadmap',
+    isCompleted: false,
+    createdAt: new Date(1722204000000).toISOString(),
+  },
+  {
+    id: '6',
+    title: 'Read new article on AI trends',
+    memo: 'The Verge just posted an interesting analysis on GPT-5',
+    isCompleted: false,
+    createdAt: new Date(1722205000000).toISOString(),
+  },
+]
+
 export const handlers = [
-  http.get('/api/todos', () => {
-    const Todos: Todo[] = [
-      {
-        id: '1',
-        title: 'Buy groceries',
-        memo: 'Milk, eggs, bread, and orange juice',
-        isCompleted: false,
-        createdAt: 1722200000000,
-      },
-      {
-        id: '2',
-        title: 'Send project update',
-        memo: 'Email the client about the current status and upcoming tasks',
-        isCompleted: true,
-        createdAt: 1722201000000,
-      },
-      {
-        id: '3',
-        title: 'Workout',
-        memo: '1 hour at the gym: cardio and legs',
-        isCompleted: false,
-        createdAt: 1722202000000,
-      },
-      {
-        id: '4',
-        title: 'Call mom',
-        memo: 'Check in and ask about weekend plans',
-        isCompleted: true,
-        createdAt: 1722203000000,
-      },
-      {
-        id: '5',
-        title: 'Prepare meeting slides',
-        memo: 'Include Q2 results and marketing roadmap',
-        isCompleted: false,
-        createdAt: 1722204000000,
-      },
-      {
-        id: '6',
-        title: 'Read new article on AI trends',
-        memo: 'The Verge just posted an interesting analysis on GPT-5',
-        isCompleted: false,
-        createdAt: 1722205000000,
-      },
-    ]
-    return HttpResponse.json(Todos)
+  http.get(paths.app.todos.path, () => {
+    return HttpResponse.json(todos)
+  }),
+
+  http.post(paths.app.todos.path, async ({ request }) => {
+    const newTodo = (await request.json()) as CreateNewTodo
+    const todoWithId: Todo = { ...newTodo, id: uuid(), createdAt: new Date().toISOString() }
+    todos.push(todoWithId)
+    return HttpResponse.json(todoWithId)
   }),
 ]
