f947bcd54ef8473347f64d98c2fa166a2dc0d6f3
Yuji Nakamura <ny5117@icloud.com>
2025-07-29 21:11:30 +0800

    implement  updateTodoCompletion logic

--- src/api/todo.ts
+++ src/api/todo.ts
@@ -1,6 +1,6 @@
 import { paths } from '../config/paths'
 
-import type { ApiResponse, CreateNewTodo, Todo } from '../types'
+import type { ApiResponse, CreateNewTodoPayload, Todo, UpdateTodoCompletionPayload } from '../types'
 
 export const getTodos = async (): Promise<ApiResponse<Todo[]>> => {
   try {
@@ -17,7 +17,7 @@ export const getTodos = async (): Promise<ApiResponse<Todo[]>> => {
   }
 }
 
-export const createNewTodo = async (newTodo: CreateNewTodo): Promise<ApiResponse<Todo>> => {
+export const createNewTodo = async (newTodo: CreateNewTodoPayload): Promise<ApiResponse<Todo>> => {
   try {
     const response = await fetch(paths.app.todos.path, {
       method: 'POST',
@@ -31,7 +31,29 @@ export const createNewTodo = async (newTodo: CreateNewTodo): Promise<ApiResponse
 
     return decodedData
   } catch (error) {
-    console.log('getTodos error:', error)
+    console.log('createNewTodo error:', error)
     throw new Error('Failed to create new todo')
+  }
+}
+
+export const updateTodoCompletion = async (
+  payload: UpdateTodoCompletionPayload,
+): Promise<ApiResponse<Todo>> => {
+  try {
+    const { id, isCompleted } = payload
+    const response = await fetch(`${paths.app.todos.path}:${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ isCompleted }),
+    })
+
+    const decodedData: ApiResponse<Todo> = await response.json()
+
+    return decodedData
+  } catch (error) {
+    console.log('updateTodoCompletio error:', error)
+    throw new Error('Failed to update todo completion')
   }
 }
