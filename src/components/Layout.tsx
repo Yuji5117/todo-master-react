677054ba6f128b9063d0c820242d849ff150a3af
Yuji Nakamura <ny5117@icloud.com>
2025-07-30 15:38:36 +0800

    fix style

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,7 +10,7 @@ export const Layout: React.FC<LayoutProps> = ({ children }) => {
       <header className="bg-primary flex h-12 items-center justify-center">
         <h1 className="text-xl font-semibold text-white">Todo Master</h1>
       </header>
-      <main className="overflow-y-aut relative flex-1">{children}</main>
+      <main className="overflow-y-auto relative flex-1">{children}</main>
     </div>
   )
 }
