b8c948947a9af6ef24023fa04fd9ac76aaeae729
Yuji Nakamura <ny5117@icloud.com>
2025-07-30 13:21:29 +0800

    show error in update form when title is empty

--- src/components/UpdateTodoForm.tsx
+++ src/components/UpdateTodoForm.tsx
@@ -17,6 +17,7 @@ export const UpdateTodoForm: React.FC<UpdateTodoFormProps> = ({ id, title, memo,
   const queryClient = useQueryClient()
   const [editedTitle, setEditedTitle] = useState<string>(title)
   const [editedMemo, setEditedMemo] = useState<string>(memo ?? '')
+  const [error, setError] = useState<string>('')
 
   const mutation = useMutation({
     mutationFn: updateTodo,
@@ -34,19 +35,27 @@ export const UpdateTodoForm: React.FC<UpdateTodoFormProps> = ({ id, title, memo,
       memo: editedMemo,
     }
 
+    if (updatedTodo.title.length === 0) {
+      setError('Title is required')
+      return
+    }
+
     mutation.mutate(updatedTodo)
   }
   return (
     <>
       <h2 className="mb-8 text-center text-2xl font-semibold">Update Todo</h2>
       <form onSubmit={handleFormSubmit} className="mb-4 flex flex-col space-y-8">
-        <input
-          type="text"
-          placeholder="Enter your todo"
-          value={editedTitle}
-          onChange={e => setEditedTitle(e.target.value)}
-          className="focus:ring-opacity-50 w-full rounded-md border border-gray-300 px-4 py-2 text-sm shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200"
-        />
+        <div>
+          <input
+            type="text"
+            placeholder="Enter your todo"
+            value={editedTitle}
+            onChange={e => setEditedTitle(e.target.value)}
+            className="focus:ring-opacity-50 w-full rounded-md border border-gray-300 px-4 py-2 text-sm shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200"
+          />
+          {error && <p className="text-error mt-1 px-4 text-sm">{error}</p>}
+        </div>
         <textarea
           rows={4}
           placeholder="Add a memo"
