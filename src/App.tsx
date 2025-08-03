f9435f5c21cebade112fd73bf2e2d0e4b2192911
Yuji Nakamura <ny5117@icloud.com>
2025-08-03 17:41:04 +0800

    move status display for rodo list to todo section component

--- src/App.tsx
+++ src/App.tsx
@@ -2,46 +2,24 @@ import { useQuery } from '@tanstack/react-query'
 import { useState } from 'react'
 
 import { getTodos } from './api/todos'
-import { TodoList } from './components/TodoList'
-import { LoadingSpinner } from './components/ui/LoadingSpinner'
+import { TodoSection } from './components/TodoSection'
 import { SearchBar } from './components/ui/SearchBar'
 
 import type { Todo } from './types'
 
 export const App: React.FC = () => {
-  const { isPending, data, isError, error } = useQuery({ queryKey: ['todos'], queryFn: getTodos })
-  const [showCompleted, setShowCompleted] = useState<boolean>(true)
   const [query, setQuery] = useState<string>('')
+  const { isPending, data, isError, error } = useQuery({
+    queryKey: ['todos', query],
+    queryFn: () => getTodos(query),
+  })
+  const [showCompleted, setShowCompleted] = useState<boolean>(true)
 
   const todos: Todo[] = data?.data ?? []
 
   const visibleTodos: Todo[] = showCompleted
     ? [...todos].sort((a, b) => Number(a.isCompleted) - Number(b.isCompleted))
     : [...todos].filter(todo => !todo.isCompleted)
-
-  if (isPending) {
-    return <LoadingSpinner />
-  }
-
-  if (isError) {
-    return (
-      <div className="text-error flex flex-col items-center justify-center py-12">
-        <h2 className="mb-2 text-xl font-semibold">Something went wrong</h2>
-        <p className="text-sm text-gray-600">Error message: {error.message}</p>
-      </div>
-    )
-  }
-
-  if (data.data.length === 0) {
-    return (
-      <div className="flex flex-col items-center justify-center py-12 text-gray-500">
-        <p className="mb-4 text-xl">You have no todos yet.</p>
-        <p className="text-sm">
-          Click the <span className="font-bold">+</span> button to add your first task.
-        </p>
-      </div>
-    )
-  }
 
   return (
     <div className="m-auto flex w-full flex-col space-y-4 px-8 py-8">
@@ -58,7 +36,8 @@ export const App: React.FC = () => {
           <div className="absolute top-0.5 left-0.5 h-5 w-5 translate-x-0 transform rounded-full bg-white shadow duration-200 peer-checked:translate-x-5" />
         </label>
       </div>
-      <TodoList todos={visibleTodos} />
+      <div className=""></div>
+      <TodoSection isLoading={isPending} isError={isError} error={error} todos={visibleTodos} />
     </div>
   )
 }
---
+++ src/components/TodoSection.tsx
@@ -0,0 +1,31 @@
+import { TodoList } from './TodoList'
+import { LoadingSpinner } from './ui/LoadingSpinner'
+
+import type { Todo } from '../types'
+
+type TodoSectionProps = {
+  isLoading: boolean
+  isError: boolean
+  error?: Error | null
+  todos: Todo[]
+}
+export const TodoSection: React.FC<TodoSectionProps> = ({ isLoading, isError, error, todos }) => {
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center pt-12">
+        <LoadingSpinner />
+      </div>
+    )
+  }
+
+  if (isError) {
+    return (
+      <div className="text-error flex flex-col items-center justify-center py-12">
+        <h2 className="mb-2 text-xl font-semibold">Something went wrong</h2>
+        <p className="text-sm text-gray-600">Error message: {error?.message}</p>
+      </div>
+    )
+  }
+
+  return <TodoList todos={todos} />
+}
