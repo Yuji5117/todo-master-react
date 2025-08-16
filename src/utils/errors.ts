cc3ad61657cb5f3dd7ef8599677a04b906cc486a
Yuji Nakamura <ny5117@icloud.com>
2025-08-16 14:35:41 +0800

    add correct argument to rapper function for ApiError

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -13,12 +13,12 @@ export class ApiError extends Error {
 }
 
 export const badRequest = (message: string, errorCode: ErrorCode): never => {
-  throw new ApiError({ message, statusCode: 400, errorCode })
+  throw new ApiError({ success: false, message, statusCode: 400, errorCode, data: null })
 }
 
 export const notFound = (message: string, errorCode: ErrorCode): never => {
-  throw new ApiError({ message, statusCode: 404, errorCode })
+  throw new ApiError({ success: false, message, statusCode: 404, errorCode, data: null })
 }
 export const internalError = (message: string, errorCode: ErrorCode): never => {
-  throw new ApiError({ message, statusCode: 500, errorCode })
+  throw new ApiError({ success: false, message, statusCode: 500, errorCode, data: null })
 }
